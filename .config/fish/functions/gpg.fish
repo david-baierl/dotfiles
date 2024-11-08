8108cbc8c563044c47a7f63ba81dcbe54ee7db07
David Baierl <12718257+david-baierl@users.noreply.github.com>
2024-11-08 09:22:16 +0100

    Initial Commit

---
+++ .config/fish/functions/gpg.fish
@@ -0,0 +1,4 @@
+function gpg --wraps='/mnt/c/Program Files (x86)/GnuPG/bin/gpg.exe' --wraps=/mnt/c/Program\\\ Files\\\ \\\(x86\\\)/GnuPG/bin/gpg.exe --description alias\ gpg=/mnt/c/Program\\\ Files\\\ \\\(x86\\\)/GnuPG/bin/gpg.exe
+  /mnt/c/Program\ Files\ \(x86\)/GnuPG/bin/gpg.exe $argv
+        
+end
