25ee53d1de70393d0e7522603bedf75635c3fc67
David Baierl <12718257+david-baierl@users.noreply.github.com>
2025-02-02 07:42:10 +0100

    update new gpg path to work with new devcontainer

--- .config/fish/functions/gpg.fish
+++ .config/fish/functions/gpg.fish
@@ -1,4 +1,4 @@
-function gpg --wraps='/mnt/c/Program Files (x86)/GnuPG/bin/gpg.exe' --wraps=/mnt/c/Program\\\ Files\\\ \\\(x86\\\)/GnuPG/bin/gpg.exe --description alias\ gpg=/mnt/c/Program\\\ Files\\\ \\\(x86\\\)/GnuPG/bin/gpg.exe
-  /mnt/c/Program\ Files\ \(x86\)/GnuPG/bin/gpg.exe $argv
-        
+# do not try to mount into windows when inside docker
+if test -z "$RUNNING_IN_DOCKER"
+    alias gpg="/mnt/c/Program\ Files\ \(x86\)/GnuPG/bin/gpg.exe"
 end
